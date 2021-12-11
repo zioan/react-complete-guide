bc9180332fe547d04cb83cce417b496749a38500
Zaharia Ioan <67908472+zioan@users.noreply.github.com>
2021-12-11 18:33:36 +0100

    Composition (children props)

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import "./Card.css";
+
+function Card(props) {
+  //Composition (children props)
+  //this will allow access to classes asigned to the component in parent components
+  //ex  <Card className="expense-item">
+
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
